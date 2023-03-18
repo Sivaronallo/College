a6a86ae29e09f0d5112bc79a2a12db0c3308c837
siva <siva_chandar@rocketmail.com>
2023-03-18 15:04:56 +0530

    Header Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule} from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from './modules/shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    SharedModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import { HeaderComponent } from './layout/header/header.component';
+
+@NgModule({
+    declarations:[
+        HeaderComponent
+    ],
+    exports:[
+        HeaderComponent,
+    ]
+})
+export class SharedModule {}
